c97a3cf50bfd14d47e717dabfe0039d5f9154e99
J.Miskolci <miskolci.joseph@live.com>
2019-11-06 00:31:06 +0100

    Revert "Add files via upload"
    This reverts commit 174ef96524987ad49f47b4f6ab146cbf95d2fd36.

--- test1/controller/controllers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require('controller/HomeController.php');
-require('controller/PostController.php');
-require('controller/AdminController.php');
-require('controller/MembersController.php');
