80345b8e7ba8606a9a461cadf91a82fd1d390ebf
J.Miskolci <miskolci.joseph@live.com>
2019-12-31 14:30:31 +0100

    Revert "Final Changes"
    This reverts commit f05da8d24cdea97c061f939dd477d7ec1ef2f4cd.

--- P4_01_code/controller/controllers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require('controller/HomeController.php');
-require('controller/CommentsController.php');
-require('controller/AdminController.php');
-require('controller/MembersController.php');
--- P4_01_code/view/error.php
+++
@@ -1,4 +0,0 @@
-<section class="ErrorMessage">
-  <i class="fas fa-exclamation-triangle"></i>
-  <p class="ErrorText"> Désolé, cette page n'existe pas !</p>
-</section>
